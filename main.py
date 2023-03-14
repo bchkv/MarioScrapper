9584a08133a88fcdd6327c22713f1d69fa33eb26
hyper <hyperskill@hyperskill.org>
2023-03-14 00:36:38 -0600

    Changed behaviour in console output.

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ import re
 from pathlib import Path
 import requests
 from bs4 import BeautifulSoup
+import cursor
 
-print(__name__)
 
 class WrongPasswordError(Exception):
     pass
@@ -22,6 +22,8 @@ page_with_tables = "http://www.edumich.gob.mx/sigem_tel/sisat_registro_2223/" \
 
 Path(f"tables").mkdir(parents=True, exist_ok=True)
 
+cursor.hide()
+
 
 class School:
     table_count = int(0)
@@ -33,52 +35,49 @@ class School:
 
     def get_tables(self):
         with requests.session() as session:
-            print(f"Fetching tables for the school {self.name}...")
-            logging.debug(f"Fetching the main page for the school {self.name}...")
+            # print(f"Fetching tables for the school {self.name}...")
             response = session.post(login_url, data={'inputEmail': '',
                                                      'inputPassword': self.password, 'grabar': 'si'})
-            logging.debug(f"Response status: {response.status_code}")
             response.raise_for_status()
             # Here we got to check if we logged in successfully, else raise WrongPasswordError
             # with open(f'{self.name}_main.html', 'w') as main_file:
             #     main_file.write(response.text)
 
-            logging.debug(f"Getting the page with list of tables...")
             response = session.get(page_with_tables, headers=headers)
-            logging.debug(f"Response status: {response.status_code}")
             response.raise_for_status()
             # with open(f'{self.name}_tables.html', 'w') as tables_file:
             #     tables_file.write(response.text)
 
             # Now we need to get urls of tables, they always have titles "Concentrado de Información"
             soup = BeautifulSoup(response.content, 'html.parser')
-            print(f"{len(soup.find_all(title='Concentrado de Información'))} tables found!")
+            number_of_tables = len(soup.find_all(title='Concentrado de Información'))
+            print(f"Found {number_of_tables} tables for the school {self.name}.")
             School.table_count += len(soup.find_all(title='Concentrado de Información'))
             for count, table_page_element in enumerate(soup.find_all(title="Concentrado de Información")):
                 table_url = table_page_element.get('href')
 
-                logging.debug(f" Going to {table_url} to fetch the table...")
                 table_page = session.get(table_url, headers=headers)
-                logging.debug(f"Response status: {table_page.status_code}")
                 table_page.raise_for_status()
 
                 table_page_soup = BeautifulSoup(table_page.content, 'html.parser')
                 download_table_url = table_page_soup.find(class_="btn btn-warning btn-sm").get('href')
-                print(f"Downloading table {count + 1} from {download_table_url}...")
-                logging.debug(f"Downloading the table...")
+                print(f"Downloading table {count + 1} of {number_of_tables}...", end='')
+                print('\r', end='')
+                # print(f"Downloading table {count + 1} from {download_table_url}...")
                 table_file = session.get(download_table_url, allow_redirects=True, stream=True)
                 file_name = re.search(f'^.*="(.+)"$', table_file.headers['Content-Disposition']).group(1)
-                print(f"Saving table {count + 1} in the file {self.name}_{file_name}...")
+                # print(f"Saving table {count + 1} in the file {self.name}_{file_name}...")
                 with open(f"tables/{self.name}_{file_name}", 'wb') as file:
                     file.write(table_file.content)
                     School.table_count_check += 1
 
 
+print("Press Ctrl + C to exit.")
+
 list_of_logins = list()
 with open("logins.txt", 'r') as file:
     for line in file:
         list_of_logins.append(line.strip())
-
 
 for login in list_of_logins:
     School(login).get_tables()
